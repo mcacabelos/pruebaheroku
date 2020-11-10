e0f5d73ba4069aa20ed38d1421c9f37e1067b530
Razinger48 <ferrialexis@gmail.com>
2020-11-10 20:15:22 -0300

    Pagina registro

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,14 +1,124 @@
-import React, { Component } from "react";
-//import '../assets/sass/session.scss';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import HomeNavbar from "../components/HomeNavbar";
+import Logo from "../assets/img/brand_EasyDinner.png";
+import { Link } from "react-router-dom";
 
-class Register extends Component {
-  render() {
-    return (
-      <section>
-        REGISTRO
-      </section>
-    );
-  }
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(12),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+    <HomeNavbar />
+      <CssBaseline />
+      <div className={classes.paper}>
+        <img src={Logo} />
+        <Typography component="h1" variant="h5">
+          ¡Registrate!
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="nom"
+                name="nombre"
+                variant="outlined"
+                required
+                fullWidth
+                id="primerNombre"
+                label="Nombre"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="apellido"
+                label="Apellido"
+                name="apellido"
+                autoComplete="apellido"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="contraseña"
+                label="Contraseña"
+                type="contraseña"
+                id="contraseña"
+                autoComplete="contraseña-actual"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="¡Quiero recibir promociones de EasyDinner!."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="inherit"
+            className={classes.submit}
+            style={{ color: "#16817A", fontWeight: "bold" }}
+          >
+            Registrarse
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link className="login-link " to="/login" variant="body2">
+                ¿Ya tenés una cuenta? Loggeate!
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 }
-
-export default Register;
